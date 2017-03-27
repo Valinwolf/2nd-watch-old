1e2c09dc35f0a973fbe43574747bf00166d14455
Patrick Thomas <admin@twprogrammers.com>
2017-03-27 00:01:37 -0500

    Test #8

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -8,34 +8,34 @@ rocky.on('draw', function(e){
 	var bgW = ctx.canvas.unobstructedWidth;
 	var dim;
 	var d = new Date();
-	//var date = d.toLocaleDateString();
-	//var time = d.toLocaleTimeString();
-	//ctx.clearRect(0,0,cliW,cliH);
-	//ctx.lineWidth = 10;
+	var date = 'Monday, Mar 27';//d.toLocaleDateString();
+	var time = '12:01 AM';//d.toLocaleTimeString();
+	ctx.clearRect(0,0,cliW,cliH);
+	ctx.lineWidth = 10;
 	
 	// Red
-	//ctx.strokeStyle = 'red';
-	//ctx.strokeRect(0,0,bgW,bgH);
+	ctx.strokeStyle = 'red';
+	ctx.strokeRect(0,0,bgW,bgH);
 	
 	// White
-	//ctx.strokeStyle = 'white';
-	//ctx.strokeRect(10,10,bgW-20,bgH-20);
+	ctx.strokeStyle = 'white';
+	ctx.strokeRect(10,10,bgW-20,bgH-20);
 	
 	// Blue
-	//ctx.strokeStyle = 'blue';
-	//ctx.strokeRect(20,20,bgW-40,bgH-40);
+	ctx.strokeStyle = 'blue';
+	ctx.strokeRect(20,20,bgW-40,bgH-40);
 	
-	//ctx.textAlign = 'center';
+	ctx.textAlign = 'center';
 	
 	// Time
-	//ctx.font = '30px bolder Bitham';
-	//dim = ctx.measureText(time);
-	//ctx.fillText(time, bgW / 2 - dim.width / 2,45,bgW);
+	ctx.font = '30px bolder Bitham';
+	dim = ctx.measureText(time);
+	ctx.fillText(time, bgW / 2 - dim.width / 2,45,bgW);
 	
 	// Date
-	//ctx.font = '18px Gothic';
-	//dim = ctx.measureText(date);
-	//ctx.fillText(date, bgW / 2 - dim.width / 2,76,bgW);
+	ctx.font = '18px Gothic';
+	dim = ctx.measureText(date);
+	ctx.fillText(date, bgW / 2 - dim.width / 2,76,bgW);
 	
 	
 });
